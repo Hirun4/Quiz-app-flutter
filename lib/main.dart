bd881bd90478f55d95bddbe0952b311da79986da
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-14 13:24:00 +0530

    main change for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/pages/home.dart';
 import 'package:quiz_app/pages/onboarding.dart';
 
 void main() {
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Onboarding(),
+      home: Home(),
     );
   }
 }
