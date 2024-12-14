82fc6e4402b1a96dc50ad89f505f58cf30dad351
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-14 11:55:15 +0530

    image

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -17,6 +17,7 @@ class _OnboardingState extends State<Onboarding> {
             Container(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
+              child: Image.asset("images/background.png"),
             )
           ],
         ),
