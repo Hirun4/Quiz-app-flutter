01884c4f27bd7633891a9e8ada3f2a88bef7b478
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-16 11:48:06 +0530

    tapping answer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -429,66 +429,90 @@ class _HomeState extends State<Home> {
                               SizedBox(
                                 height: 30.0,
                               ),
-                              Container(
-                                height: 50.0,
-                                margin:
-                                    EdgeInsets.only(left: 20.0, right: 20.0),
-                                width: MediaQuery.of(context).size.width,
-                                decoration: BoxDecoration(
-                                    border: Border.all(width: 2.0),
-                                    borderRadius: BorderRadius.circular(20.0)),
-                                child: Center(
-                                  child: Text(
-                                    option[0].replaceAll(RegExp(r'[\[\]]'), ""),
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight.bold),
+                              GestureDetector(
+                                onTap: () {
+                                  answernow = true;
+                                  setState(() {});
+                                },
+                                child: Container(
+                                  height: 50.0,
+                                  margin:
+                                      EdgeInsets.only(left: 20.0, right: 20.0),
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                      border: Border.all(width: 2.0),
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                  child: Center(
+                                    child: Text(
+                                      option[0]
+                                          .replaceAll(RegExp(r'[\[\]]'), ""),
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
                                   ),
                                 ),
                               ),
                               SizedBox(
                                 height: 20.0,
                               ),
-                              Container(
-                                height: 50.0,
-                                margin:
-                                    EdgeInsets.only(left: 20.0, right: 20.0),
-                                width: MediaQuery.of(context).size.width,
-                                decoration: BoxDecoration(
-                                    border: Border.all(width: 2.0),
-                                    borderRadius: BorderRadius.circular(20.0)),
-                                child: Center(
-                                  child: Text(
-                                    option[1].replaceAll(RegExp(r'[\[\]]'), ""),
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight.bold),
+                              GestureDetector(
+                                onTap: () {
+                                  answernow = true;
+                                  setState(() {});
+                                },
+                                child: Container(
+                                  height: 50.0,
+                                  margin:
+                                      EdgeInsets.only(left: 20.0, right: 20.0),
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                      border: Border.all(width: 2.0),
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                  child: Center(
+                                    child: Text(
+                                      option[1]
+                                          .replaceAll(RegExp(r'[\[\]]'), ""),
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
                                   ),
                                 ),
                               ),
                               SizedBox(
                                 height: 20.0,
                               ),
-                              Container(
-                                height: 50.0,
-                                margin:
-                                    EdgeInsets.only(left: 20.0, right: 20.0),
-                                width: MediaQuery.of(context).size.width,
-                                decoration: BoxDecoration(
-                                    border: Border.all(width: 2.0),
-                                    borderRadius: BorderRadius.circular(20.0)),
-                                child: Center(
-                                  child: Text(
-                                    option[2].replaceAll(RegExp(r'[\[\]]'), ""),
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight.bold),
+                              GestureDetector(
+                                onTap: () {
+                                  answernow = true;
+                                  setState(() {});
+                                },
+                                child: Container(
+                                  height: 50.0,
+                                  margin:
+                                      EdgeInsets.only(left: 20.0, right: 20.0),
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                      border: Border.all(width: 2.0),
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                  child: Center(
+                                    child: Text(
+                                      option[2]
+                                          .replaceAll(RegExp(r'[\[\]]'), ""),
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
                                   ),
                                 ),
                               ),
